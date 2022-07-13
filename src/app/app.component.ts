0c9e27e5358c1dbcc3016b279c4832b69fb3279f
quynhtrang0605 <trang06052000@gmail.com>
2022-07-13 16:12:42 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+
+  // data binding
+  // inputType = 'text';
+
+  // tạo 1 obj mới
+  // nếu để private user thì sẽ báo lỗi vì k trích xuất được
+  user = {
+    name: 'Trang',
+    age: 22,
+  };
+
+  handler(event) {
+    console.log('clicked', event);
+  }
 }
